b937117420be9bd2171bf5fa9969e94dd0d52922
Anton <anton27081986@gmail.com>
2021-07-06 15:11:22 +0300

    add JSON-server

--- js/script.js
+++ js/script.js
@@ -305,6 +305,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
     
 });
 
